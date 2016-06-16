0bc4021903c7ccae7120de76a3b5bb8614739a7b
Gaita Joe <gaitajoe@gmail.com>
2016-06-16 18:58:09 +0300

    .

--- day2/OOP/test_Noteapplication.py
+++ day2/OOP/test_Noteapplication.py
@@ -1,19 +1,21 @@
 import unittest
-import Noteapplication
-#from fizzbuzz import fizz_buzz
+import Note-it
+#from Note-it import Noteapplication
 
 class NoteapplicationTest(unittest.TestCase):
 	"""Testing the Noteapplication class"""
 	""" """
-	def create_new_note(self):
+	def if_able_to_create_new_note_and_add_to_note_list(self):
 		"""Test if function can successfully append new note to note_list"""
-		self.assertEqual(fizzbuzz.fizz_buzz(3), 'fizz')
-		#self.assertEqual(fizz_buzz(3), 'fizz')
+		self.assertEqual(Note-it.create_note(3), 'My Fourth')
+		#self.assertEqual(Search_note(3), 'My Third')
 
-	def search_te(self):
+	def search_note_list_(self):
 		"""Test returns buzz when input is divisible by five"""
-		self.assertEqual(fizzbuzz.fizz_buzz(3), 'fizz')
-		#self.assertEqual(fizz_buzz(5), 'fizz')
+		self.assertEqual(Note-it.Search_note(3), 'My Second')
+		#self.assertEqual(Search_note(5), 'My Third')
+
+
 
 #if __name__ == ('__main__'):
 #	unittest()
